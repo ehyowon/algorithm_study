fb9a3cbf7d10f8c55150f5e8e841447075fb026d
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-07-02 21:35:03 +0900

    Create Sol2.py

---
+++ Silver/18870_좌표압축/Sol2.py
@@ -0,0 +1,18 @@
+N = int(input())
+X = list(map(int, input().split()))
+Y = set(X)
+Z = [0 for _ in range(N)]
+a = []
+
+k = 0
+for i in range(N):
+	m = min(X)
+	if k > 0 and m == a[-1]:
+		k -= 1
+	a.append(m)
+	b = X.index(a[i])
+	X[b] = float("inf")
+	Z[b] = k
+	k += 1
+
+print(*Z)
