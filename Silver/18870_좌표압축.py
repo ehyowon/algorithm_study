73b7f6c512dc85aebe61d363315092917fa4f432
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-07-02 19:36:26 +0900

    Create 18870_좌표압축.py

---
+++ Silver/18870_좌표압축.py
@@ -0,0 +1,13 @@
+N = int(input())
+X = list(map(int, input().split()))
+idx = sorted(X)
+
+for i in range(N):
+	k = 0
+	for j in range(N) : 
+		if j >=1 and idx[j] == idx[j-1] : 
+			k -= 1
+		if X[i] == idx[j] : 
+			print(k, end=" ")
+			break
+		k += 1
