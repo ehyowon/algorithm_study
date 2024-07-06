ef3056fc1cc6b552009c440e706c9597fd66363d
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-07-06 20:48:26 +0900

    Update Sol.py

--- Silver/1193_분수찾기/Sol.py
+++ Silver/1193_분수찾기/Sol.py
@@ -1,32 +1,32 @@
 def move(a, b):
-	if a %2 == 1 :
-		if a == 1:
-			if b % 2 == 0:
-				a += 1
-				b -= 1
-			elif b % 2 == 1:
-				b += 1
-			return a, b
-	if b == 1:
-			if a % 2 == 0:
-				a += 1
-			elif a % 2 == 1:
-				a -= 1
-				b += 1
-			return a, b
-	if (a + b) % 2 == 0:
-		a -= 1
-		b += 1
-		return a, b
-	if (a + b) % 2 == 1:
+    if a %2 == 1 :
+	if a == 1:
+            if b % 2 == 0:
 		a += 1
 		b -= 1
-		return a, b
+	    elif b % 2 == 1:
+		b += 1
+	    return a, b
+        if b == 1:
+	    if a % 2 == 0:
+		a += 1
+	    elif a % 2 == 1:
+		a -= 1
+		b += 1
+	    return a, b
+    if (a + b) % 2 == 0:
+	a -= 1
+	b += 1
+	return a, b
+    if (a + b) % 2 == 1:
+	a += 1
+	b -= 1
+	return a, b
 
 x = int(input())
 i, j = 1, 1
 
 for _ in range(x-1):
-	i, j = move(i, j)
+    i, j = move(i, j)
 
 print('{}/{}'.format(i, j))
