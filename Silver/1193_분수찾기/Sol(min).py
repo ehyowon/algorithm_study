e503d8a53f01148ebe28cb316352a1413c240a88
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-07-08 13:35:00 +0900

    Create Sol(min).py

---
+++ Silver/1193_분수찾기/Sol(min).py
@@ -0,0 +1,16 @@
+# 민채가 쓴 코드인데 대각선 한 줄을 line으로 생각해서 규칙 찾아서 품!
+num = int(input())
+line = 1
+
+while num > line:
+        num -= line
+        line += 1
+i = 1
+if line % 2 == 1:
+        a = line - num + 1
+        b = num
+elif line % 2 == 0: 
+        a = num
+        b = line - num + 1
+        
+print(f'{a}/{b}')
